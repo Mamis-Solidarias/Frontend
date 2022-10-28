ff6520828014762514446a1862f6f2d9de06e82f
Ignacio <isampedro@itba.edu.ar>
2022-10-28 19:49:55 -0300

    Added important functions to permission grant

--- src/utils/sessionManagement.ts
+++ src/utils/sessionManagement.ts
@@ -1,5 +1,6 @@
 import { NextRouter } from 'next/router';
 import Cookies from 'js-cookie';
+import Role from 'src/types/Role';
 
 export const userIsLoggedIn = () => {
   return !!Cookies.get('MamisSolidarias.Auth');
@@ -17,4 +18,32 @@ export const hasNoPermission = (err: any) => {
 // Not Logged in
 export const isNotLoggedIn = (err: any) => {
   return err.status === 401;
+};
+
+export const getUserRolesLocal = () => {
+  return userIsLoggedIn() ? JSON.parse(localStorage.getItem('user') as string).roles : null;
+};
+
+export const hasWriteAccess = (service: string) => {
+  const adminRoles = getUserRolesLocal();
+  let hasIt = false;
+  adminRoles.forEach((role: Role) => {
+    if (role.service === service && role.canWrite) {
+      hasIt = true;
+    }
+  });
+
+  return hasIt;
+};
+
+export const hasReadAccess = (service: string) => {
+  const adminRoles = getUserRolesLocal();
+  let hasIt = false;
+  adminRoles.forEach((role: Role) => {
+    if (role.service === service && role.canRead) {
+      hasIt = true;
+    }
+  });
+
+  return hasIt;
 };
