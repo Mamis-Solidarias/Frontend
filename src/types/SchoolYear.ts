4e311cccd2f8589c120a327eb29faf49031f6194
isampedro <42911632+isampedro@users.noreply.github.com>
2022-10-28 17:51:27 -0300

    Minor bug Fix (#89)

--- src/types/SchoolYear.ts
+++ src/types/SchoolYear.ts
@@ -1,20 +1,20 @@
 export const SCHOOL_YEARS = {
+  PRE_SCHOOL3: 'Salita de 3, Pre escolar',
+  PRE_SCHOOL4: 'Salita de 4, Pre escolar',
+  PRE_SCHOOL5: 'Salita de 5, Pre escolar',
+  PRIMARY_SCHOOL1: 'Escuela Primaria, 1er Año',
+  PRIMARY_SCHOOL2: 'Escuela Primaria, 2do Año',
+  PRIMARY_SCHOOL3: 'Escuela Primaria, 3er Año',
+  MIDDLE_SCHOOL4: 'Escuela Primaria, 4to Año',
+  MIDDLE_SCHOOL5: 'Escuela Primaria, 5to Año',
+  MIDDLE_SCHOOL6: 'Escuela Primaria, 6to Año',
+  MIDDLE_SCHOOL7: 'Escuela Primaria, 7mo Año',
   HIGH_SCHOOL1: 'Escuela Secundaria, 1er Año',
   HIGH_SCHOOL2: 'Escuela Secundaria, 2do Año',
   HIGH_SCHOOL3: 'Escuela Secundaria, 3er Año',
   HIGH_SCHOOL4: 'Escuela Secundaria, 4to Año',
   HIGH_SCHOOL5: 'Escuela Secundaria, 5to Año',
-  HIGH_SCHOOL6: 'Escuela Secundaria, 6to Año',
-  MIDDLE_SCHOOL4: 'Escuela Primaria, 4to Año',
-  MIDDLE_SCHOOL5: 'Escuela Primaria, 5to Año',
-  MIDDLE_SCHOOL6: 'Escuela Primaria, 6to Año',
-  MIDDLE_SCHOOL7: 'Escuela Primaria, 7mo Año',
-  PRIMARY_SCHOOL1: 'Escuela Primaria, 1er Año',
-  PRIMARY_SCHOOL2: 'Escuela Primaria, 2do Año',
-  PRIMARY_SCHOOL3: 'Escuela Primaria, 3er Año',
-  PRE_SCHOOL3: 'Salita de 3, Pre escolar',
-  PRE_SCHOOL4: 'Salita de 4, Pre escolar',
-  PRE_SCHOOL5: 'Salita de 5, Pre escolar'
+  HIGH_SCHOOL6: 'Escuela Secundaria, 6to Año'
 };
 
 export const SCHOOL_CYCLES = {
