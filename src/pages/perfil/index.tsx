01570ae1a0cbd10931a4b00abdc97bd65160ed51
Ignacio <isampedro@itba.edu.ar>
2022-11-28 01:00:39 -0300

    Removed redirect to login from profile

--- src/pages/perfil/index.tsx
+++ src/pages/perfil/index.tsx
@@ -1,28 +1,17 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Portal from '@mui/material/Portal';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 // ** Styled Component Import
 import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts';
 import { useAction } from 'src/hooks/actionHook';
-import { userIsLoggedIn } from 'src/utils/sessionManagement';
 import ActionToast from 'src/views/pages/misc/ActionToast';
 import { UserProfileDisplay } from 'src/views/users/UserProfileDisplay';
 
 // ** Demo Components Imports
 
 const Dashboard = () => {
-  const router = useRouter();
   const { action, setAction, setCompletion } = useAction();
-
-  useEffect(() => {
-    if (!userIsLoggedIn()) {
-      router.push('/login');
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <ApexChartWrapper>
