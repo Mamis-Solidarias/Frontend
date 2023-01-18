04ba7c4d4ae4b81c86901513bed7ed0aaf6120d1
isampedro <isampedro@itba.edu.ar>
2023-01-18 00:59:21 -0300

    [Fix] Improved default card margins

--- src/views/beneficiaries/BeneficiaryCard/DefaultCard.tsx
+++ src/views/beneficiaries/BeneficiaryCard/DefaultCard.tsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 
@@ -12,37 +12,33 @@ interface DefaultCardProps {
 }
 
 export const DefaultCard: FC<DefaultCardProps> = props => {
-  const { title, fields, sx } = props;
-  
-  const style = {...{margin:"15px"},...sx};
+  const {title, fields, sx} = props;
+
+  const style = {...{marginInline: "0.5em"}, ...sx};
 
   return (
     <Card sx={style}>
-      <CardHeader title={title} titleTypographyProps={{ sx: { letterSpacing: '0.15px !important' } }} />
-      <CardContent sx={{ pt: theme => `${theme.spacing(2.0)} !important` }}>
+      <CardContent sx={{pt: theme => `${theme.spacing(2.0)} !important`}}>
+        <Typography
+          className={"MuiTypography--heading"}
+          variant={"h6"}
+        >
+          {title}
+        </Typography>
         {Object.keys(fields).map(key => (
-          <Box
-            key={key}
-            sx={{
-              width: '100%',
-              display: 'flex',
-              flexWrap: 'wrap',
-              flexDirection: 'column',
-              alignItems: 'start'
-            }}
-          >
-            <Box sx={{ marginRight: 2, display: 'flex', flexDirection: 'column' }}>
-              <Typography variant='body2' sx={{ mb: 0.5, fontWeight: 600, color: 'text.primary' }}>
-                {key}:
-              </Typography>
-            </Box>
-
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Typography variant='body2' sx={{ color: 'text.primary' }}>
-                {fields[key]}
-              </Typography>
-            </Box>
-          </Box>
+          <>
+            <Typography
+              className={"MuiTypography--heading"}
+            >
+              {key}
+            </Typography>
+            <Typography
+              className={"MuiTypography--subheading"}
+              variant={"caption"}
+            >
+              {fields[key]}
+            </Typography>
+          </>
         ))}
       </CardContent>
     </Card>
