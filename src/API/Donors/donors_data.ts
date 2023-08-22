cffd098091e8aabbf2b3e2d1c16264effa96f56a
Ignacio <isampedro@itba.edu.ar>
2023-08-22 08:36:25 -0300

    [Fix] Now edit donnor has all optional fields

--- src/API/Donors/donors_data.ts
+++ src/API/Donors/donors_data.ts
@@ -1,13 +1,20 @@
-import { Donor } from 'src/types/donors/Donor';
-import { axiosClient } from './initialization';
+import {Donor} from "src/types/donors/Donor";
+import {axiosClient} from "./initialization";
 
 export const createDonor = async (data: Donor) => {
-  return axiosClient.post('donors', data);
+  return axiosClient.post("donors", data);
 };
 
 export const updateDonor = async (
   id: string,
   data: { name?: string | null; email?: string | null; phone?: string | null; isGodFather?: boolean | null }
 ) => {
-  return axiosClient.put('donors/' + id, data);
+  const dataToSend = {
+    name: data.name === null ? undefined : data.name,
+    email: data.email === null ? undefined : data.email,
+    phone: data.phone === null ? undefined : data.phone,
+    isGodFather: data.isGodFather === null ? undefined : data.isGodFather
+  };
+
+  return axiosClient.put("donors/" + id, dataToSend);
 };
